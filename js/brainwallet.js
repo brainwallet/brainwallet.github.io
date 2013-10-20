337d0b2fcac75c94c2d2175e385ced51ac727acb
brainwallet <brainwallet.org@gmail.com>
2013-10-21 00:50:13 +0600

    close dropdown after selection

--- js/brainwallet.js
+++ js/brainwallet.js
@@ -1157,6 +1157,7 @@
       PRIVATE_KEY_VERSION = (PUBLIC_KEY_VERSION+128)&255;
       $('#crLabel').text($(this).text());
       gen_update();
+      $('.dropdown-toggle').dropdown('toggle');
       return false;
     }
 
