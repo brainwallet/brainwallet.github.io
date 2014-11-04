d16967e9a02a358d4aa4d1aecfc882a75e5a64f3
brainwallet <brainwallet.org@gmail.com>
2014-11-05 00:01:30 +0500

    added String.trim() to fromBin

--- js/brainwallet.js
+++ js/brainwallet.js
@@ -523,7 +523,7 @@
 
     function fromBin(str)
     {
-      var arr = str.split(/[\r\n ]+/);
+      var arr = str.trim().split(/[\r\n ]+/);
       var res = [];
       for (var i=0; i<arr.length; i++)
       {
